087efbec711aeb884e6427b6766fc6b15d5b2ea9
Tomescu Mihail <tomescumihail93@gmail.com>
2018-01-24 12:21:32 +0100

    Detail page update, created structure + dynamic cards

--- src/app/coins/coins.module.ts
+++ src/app/coins/coins.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpModule } from '@angular/http';
 import { ChartistModule } from 'ng-chartist';
+import { MatchHeightModule } from "../shared/directives/match-height.directive";
+import { ChartsModule } from 'ng2-charts';
 
 import { CoinsRoutingModule } from './coins-routing.module';
 import { CoinsComponent } from './coins.component';
@@ -10,13 +12,16 @@ import { CoinsService } from "./coins.service";
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
 
+
 @NgModule({
   imports: [
     CommonModule,
     CoinsRoutingModule,
     NgbModule,
     HttpModule,
-    ChartistModule
+    ChartistModule,
+    ChartsModule,
+    MatchHeightModule
   ],
   declarations: [CoinsComponent, ListComponent, DetailsComponent],
   providers: [
--- src/app/coins/details/details.component.ts
+++ src/app/coins/details/details.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import * as Chartist from 'chartist';
+import * as chartsData from '../../shared/data/chartjs';
 import { ChartType, ChartEvent } from "ng-chartist/dist/chartist.component";
+
 import { CoinsService } from "../coins.service";
 import { Coin } from 'app/ico/model/coin';
 
@@ -39,29 +41,296 @@ export class DetailsComponent implements OnInit {
     precent_change_24h: "",
     precent_change_7d: "",
     last_updated: ""
-}
-  // line chart configuration Starts
-  WidgetlineChart: Chart = {
-    type: 'Line', data: data['WidgetlineChart'],
+  }
+
+  // Radar
+  public radarChartLabels = chartsData.radarChartLabels;
+
+  public radarChartData = chartsData.radarChartData;
+  public radarChartType = chartsData.radarChartType;
+  public radarChartColors = chartsData.radarChartColors;
+  public radarChartOptions = chartsData.radarChartOptions;
+
+  // Line area chart 2 configuration Starts
+  lineArea2: Chart = {
+    type: 'Line',
+    data: data['lineArea2'],
     options: {
+      showArea: true,
+      fullWidth: true,
+      lineSmooth: Chartist.Interpolation.none(),
       axisX: {
-        showGrid: true,
-        showLabel: false,
-        offset: 0,
+        showGrid: false,
       },
       axisY: {
-        showGrid: false,
-        low: 40,
-        showLabel: false,
-        offset: 0,
+        low: 0,
+        scaleMinSpace: 50,
+      }
+    },
+    responsiveOptions: [
+      ['screen and (max-width: 640px) and (min-width: 381px)', {
+        axisX: {
+          labelInterpolationFnc: function (value, index) {
+            return index % 2 === 0 ? value : null;
+          }
+        }
+      }],
+      ['screen and (max-width: 380px)', {
+        axisX: {
+          labelInterpolationFnc: function (value, index) {
+            return index % 3 === 0 ? value : null;
+          }
+        }
+      }]
+    ],
+    events: {
+      created(data: any): void {
+        var defs = data.svg.elem('defs');
+        defs.elem('linearGradient', {
+          id: 'gradient2',
+          x1: 0,
+          y1: 1,
+          x2: 0,
+          y2: 0
+        }).elem('stop', {
+          offset: 0,
+          'stop-opacity': '0.2',
+          'stop-color': 'rgba(255, 255, 255, 1)'
+        }).parent().elem('stop', {
+          offset: 1,
+          'stop-opacity': '0.2',
+          'stop-color': 'rgba(0, 201, 255, 1)'
+        });
+
+        defs.elem('linearGradient', {
+          id: 'gradient3',
+          x1: 0,
+          y1: 1,
+          x2: 0,
+          y2: 0
+        }).elem('stop', {
+          offset: 0.3,
+          'stop-opacity': '0.2',
+          'stop-color': 'rgba(255, 255, 255, 1)'
+        }).parent().elem('stop', {
+          offset: 1,
+          'stop-opacity': '0.2',
+          'stop-color': 'rgba(132, 60, 247, 1)'
+        });
       },
-      lineSmooth: Chartist.Interpolation.cardinal({
-        tension: 0
-      }),
-      fullWidth: true,
+      draw(data: any): void {
+        var circleRadius = 4;
+        if (data.type === 'point') {
+
+          var circle = new Chartist.Svg('circle', {
+            cx: data.x,
+            cy: data.y,
+            r: circleRadius,
+            class: 'ct-point-circle'
+          });
+          data.element.replace(circle);
+        }
+        else if (data.type === 'label') {
+          // adjust label position for rotation
+          const dX = data.width / 2 + (30 - data.width)
+          data.element.attr({ x: data.element.attr('x') - dX })
+        }
+      }
     },
   };
-  // Line chart configuration Ends
+  // Line area chart 2 configuration Ends
+
+  // Line area chart configuration Starts
+  lineArea: Chart = {
+    type: 'Line',
+    data: data['lineAreaDashboard'],
+    options: {
+      low: 0,
+      showArea: true,
+      fullWidth: true,
+      onlyInteger: true,
+      axisY: {
+        low: 0,
+        scaleMinSpace: 50,
+      },
+      axisX: {
+        showGrid: false
+      }
+    },
+    events: {
+      created(data: any): void {
+        var defs = data.svg.elem('defs');
+        defs.elem('linearGradient', {
+          id: 'gradient',
+          x1: 0,
+          y1: 1,
+          x2: 1,
+          y2: 0
+        }).elem('stop', {
+          offset: 0,
+          'stop-color': 'rgba(0, 201, 255, 1)'
+        }).parent().elem('stop', {
+          offset: 1,
+          'stop-color': 'rgba(146, 254, 157, 1)'
+        });
+
+        defs.elem('linearGradient', {
+          id: 'gradient1',
+          x1: 0,
+          y1: 1,
+          x2: 1,
+          y2: 0
+        }).elem('stop', {
+          offset: 0,
+          'stop-color': 'rgba(132, 60, 247, 1)'
+        }).parent().elem('stop', {
+          offset: 1,
+          'stop-color': 'rgba(56, 184, 242, 1)'
+        });
+      },
+
+    },
+  };
+  // Line area chart configuration Ends
+
+  // Line chart configuration Starts
+  lineChart: Chart = {
+    type: 'Line', data: data['LineDashboard'],
+    options: {
+        axisX: {
+            showGrid: false
+        },
+        axisY: {
+            showGrid: false,
+            showLabel: false,
+            low: 0,
+            high: 100,
+            offset: 0,
+        },
+        fullWidth: true,
+        offset: 0,
+    },
+    events: {
+        draw(data: any): void {
+            var circleRadius = 4;
+            if (data.type === 'point') {
+                var circle = new Chartist.Svg('circle', {
+                    cx: data.x,
+                    cy: data.y,
+                    r: circleRadius,
+                    class: 'ct-point-circle'
+                });
+
+                data.element.replace(circle);
+            }
+            else if (data.type === 'label') {
+                // adjust label position for rotation
+                const dX = data.width / 2 + (30 - data.width)
+                data.element.attr({ x: data.element.attr('x') - dX })
+            }
+        }
+    },
+
+};
+// Line chart configuration Ends
+
+//  Bar chart configuration Starts
+BarChart: Chart = {
+  type: 'Bar', data: data['DashboardBar'], options: {
+      axisX: {
+          showGrid: false,
+      },
+      axisY: {
+          showGrid: false,
+          showLabel: false,
+          offset: 0
+      },
+      low: 0,
+      high: 60, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+  },
+  responsiveOptions: [
+      ['screen and (max-width: 640px)', {
+          seriesBarDistance: 5,
+          axisX: {
+              labelInterpolationFnc: function (value) {
+                  return value[0];
+              }
+          }
+      }]
+  ],
+  events: {
+      created(data: any): void {
+          var defs = data.svg.elem('defs');
+          defs.elem('linearGradient', {
+              id: 'gradient4',
+              x1: 0,
+              y1: 1,
+              x2: 0,
+              y2: 0
+          }).elem('stop', {
+              offset: 0,
+              'stop-color': 'rgba(238, 9, 121,1)'
+          }).parent().elem('stop', {
+              offset: 1,
+              'stop-color': 'rgba(255, 106, 0, 1)'
+          });
+          defs.elem('linearGradient', {
+              id: 'gradient5',
+              x1: 0,
+              y1: 1,
+              x2: 0,
+              y2: 0
+          }).elem('stop', {
+              offset: 0,
+              'stop-color': 'rgba(0, 75, 145,1)'
+          }).parent().elem('stop', {
+              offset: 1,
+              'stop-color': 'rgba(120, 204, 55, 1)'
+          });
+
+          defs.elem('linearGradient', {
+              id: 'gradient6',
+              x1: 0,
+              y1: 1,
+              x2: 0,
+              y2: 0
+          }).elem('stop', {
+              offset: 0,
+              'stop-color': 'rgba(132, 60, 247,1)'
+          }).parent().elem('stop', {
+              offset: 1,
+              'stop-color': 'rgba(56, 184, 242, 1)'
+          });
+          defs.elem('linearGradient', {
+              id: 'gradient7',
+              x1: 0,
+              y1: 1,
+              x2: 0,
+              y2: 0
+          }).elem('stop', {
+              offset: 0,
+              'stop-color': 'rgba(155, 60, 183,1)'
+          }).parent().elem('stop', {
+              offset: 1,
+              'stop-color': 'rgba(255, 57, 111, 1)'
+          });
+
+      },
+      draw(data: any): void {
+          var barHorizontalCenter, barVerticalCenter, label, value;
+          if (data.type === 'bar') {
+
+              data.element.attr({
+                  y1: 195,
+                  x1: data.x1 + 0.001
+              });
+
+          }
+      }
+  },
+
+};
+// Bar chart configuration Ends
 
   constructor(private route: ActivatedRoute, private coinService: CoinsService) { }
 
