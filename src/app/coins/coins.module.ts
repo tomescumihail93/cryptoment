702d8df7d371f29db40bb6fa0171ac60853d40d4
Tomescu Mihail <tomescumihail93@gmail.com>
2018-01-28 00:53:32 +0100

    Implemented search for coin list

--- src/app/coins/coins.module.ts
+++ src/app/coins/coins.module.ts
@@ -5,13 +5,13 @@ import { HttpModule } from '@angular/http';
 import { ChartistModule } from 'ng-chartist';
 import { MatchHeightModule } from "../shared/directives/match-height.directive";
 import { ChartsModule } from 'ng2-charts';
-
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from "../shared/shared.module";
 import { CoinsRoutingModule } from './coins-routing.module';
 import { CoinsComponent } from './coins.component';
 import { CoinsService } from "./coins.service";
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
-
 
 @NgModule({
   imports: [
@@ -21,7 +21,9 @@ import { DetailsComponent } from './details/details.component';
     HttpModule,
     ChartistModule,
     ChartsModule,
-    MatchHeightModule
+    MatchHeightModule,
+    FormsModule,
+    SharedModule
   ],
   declarations: [CoinsComponent, ListComponent, DetailsComponent],
   providers: [
--- src/app/coins/list/list.component.ts
+++ src/app/coins/list/list.component.ts
@@ -8,6 +8,7 @@ import { ChartType, ChartEvent } from "ng-chartist/dist/chartist.component";
 import { CoinsService } from "../coins.service";
 import { Coin } from "../metadata/coin";
 import { CoinData } from "../metadata/coin-data";
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 declare var require: any;
 
@@ -25,36 +26,28 @@ export interface Chart {
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: '0' }),
+        animate('.5s ease-out', style({ opacity: '1' })),
+      ]),
+      transition(':leave', [
+        style({ opacity: '1' }),
+        animate('.5s ease-out', style({ opacity: '0' })),
+      ]),
+    ]),
+  ]
 })
 export class ListComponent implements OnInit {
   public coins: Coin[] = [];
   public parsedData: Chart[] = [];
   public chartData: any[] = [];
   public isDataAvailable: boolean = false;
+  public searchText: string = '';
 
   bgarray = [
-    /*
-  "gradient-yellow-green",
-  "gradient-orange-deep-orange",
-  "gradient-deep-purple-purple",
-  "gradient-red-pink",
-  "gradient-light-green-amber",
-  "gradient-amber-amber",
-  "gradient-purple-pink",
-  "gradient-indigo-dark-blue",
-  "gradient-teal-cyan",
-  "gradient-blue-grey-blue-grey",
-  "gradient-cyan-dark-green",
-  "gradient-orange-amber",
-  "gradient-indigo-blue",
-  "gradient-brown-brown",
-  "gradient-blue-grey-blue",
-  "gradient-purple-deep-orange",
-  "gradient-green-teal",
-  "gradient-purple-deep-purple",
-  "gradient-deep-purple-blue"
-  */
     "gradient-green-tea",
     "gradient-blueberry",
     "gradient-king-yna",
@@ -70,30 +63,7 @@ export class ListComponent implements OnInit {
     "gradient-crystal-clear",
     "gradient-pomegranate"
   ];
-  /**
-  // line chart configuration Starts
-  WidgetlineChart: Chart = {
-    type: 'Line', data: data['WidgetlineChart'],
-    options: {
-      axisX: {
-        showGrid: true,
-        showLabel: false,
-        offset: 0,
-      },
-      axisY: {
-        showGrid: false,
-        low: 40,
-        showLabel: false,
-        offset: 0,
-      },
-      lineSmooth: Chartist.Interpolation.cardinal({
-        tension: 0
-      }),
-      fullWidth: true,
-    },
-  };
-  // Line chart configuration Ends
-**/
+
   constructor(private coinService: CoinsService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -130,11 +100,11 @@ export class ListComponent implements OnInit {
   }
 
   loadDetails(coin) {
-    this.router.navigate(['../details', coin.id], { relativeTo: this.route });
+    var id = this.chartData.find(coinData => coinData.symbol == coin.symbol).id;
+    this.router.navigate(['../details', id], { relativeTo: this.route });
   }
 
   parseData() {
-    console.log(this.chartData);
     this.chartData.forEach(dbFormattedCoin => {
       
       var localData = {
@@ -144,8 +114,6 @@ export class ListComponent implements OnInit {
 
       var low = Math.min(...dbFormattedCoin["priceUSD"]);
       var high = Math.max(...dbFormattedCoin["priceUSD"]);
-      console.log(localData)
-
 
       this.parsedData.push({
         type: 'Line', data: localData,
